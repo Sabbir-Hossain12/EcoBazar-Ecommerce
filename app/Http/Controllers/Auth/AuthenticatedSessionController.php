c799350bf970e48c395c431f41a96d348a27430c
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2024-06-15 01:45:20 +0600

    admin panel:login page added

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login');
+        return view('auth.loginv2');
     }
 
     /**
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('admin.dashboard', absolute: false));
     }
 
     /**
