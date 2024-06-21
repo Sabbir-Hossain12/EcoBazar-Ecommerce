4d6d441a189a63462066321d6a6d0ca286122464
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2024-06-22 01:48:51 +0600

    installed Spatie role and permission package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(
             [
-                'admin' => admin::class
+                'admin' => admin::class,
+                'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+                'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+                'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             ]
         );
     })
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
     
 ];
